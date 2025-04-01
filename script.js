85812eb958098747dc8a6734019f2beb544ac218
JASURBE77 <animesila89@gmail.com>
2025-04-01 17:51:03 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+let ismin = prompt('Ismingizni kiriting')
+ 
+let familiya = prompt('Familiyangizni kiriting')
+
+let kimligin = prompt('SHarifingiz')
+
+let yosh = prompt('Yoshingizni kiriting')
+
+let nomer = prompt('Telefon nomeringiz')
+
+let maktab = prompt('Qaysi maktabda oqiysiz')
+
+let email = prompt('Gmailingizni kiriting')
+
+let qiziqish = prompt('Nimalarga qiziqasiz')
+
+let kurs = prompt('Kurslaringiz')
+
+let manzil = prompt('Yashash manzilingiz')
+
+let chiqish = alert('Ismingizni kiriting:'+ ismin  + 'Familiyangizni kiriting:' + familiya + 'SHarifingiz:'+ kimligin + 'Yoshingizni kiriting:'+ yosh + 'Telefon nomeringiz'+ nomer + 'Qaysi maktabda oqiysiz'+ maktab + 'Gmailingizni kiriting'+ email + 'Nimalarga qiziqasiz'+ qiziqish + 'Kurslaringiz'+ kurs + 'Yashash manzilingiz'+ manzil)
